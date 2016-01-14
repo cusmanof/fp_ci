7b4772473da7a7f70fe6ced251127f9ca430f4f0
cusmano <cusmano@33960L.au.thalesgroup.local>
2016-01-14 12:48:26 +0800

    didn't work anyway

--- application/views/templates/footer_free.php
+++ application/views/templates/footer_free.php
@@ -1,6 +1,6 @@
 <div class="buttons" style="text-align:center;">
 <?php
-    echo '<a href="#" onclick="window.history.back();">Go back</a>';
+    echo '<a href="#" onclick="window.history.back(-1);">Go back</a>';
 ?>
 </div>
 
